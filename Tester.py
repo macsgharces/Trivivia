801247dfd420cc671d9f285cad10e2d3b2ace251
macsgharcees <49853947+macsgharces@users.noreply.github.com>
2023-09-24 22:50:59 -0400

    Updated testing class

--- Tester.py
+++ Tester.py
@@ -1,11 +1,8 @@
-import requests
 import json
-from flask import Flask
 
-app = Flask(__name__)
+import requests
 
 
-@app.route('/')
 def fact_connection():
     response = requests.get("https://cat-fact.herokuapp.com/facts/")
     connection = response.json()
@@ -13,7 +10,8 @@ def fact_connection():
     fact_dict = json.loads(fact_str)
 
     for idx, fact_dict in enumerate(fact_dict, start=1):
-        return fact_dict['text']
+        print("Text:", fact_dict['text'])
+
 
 if __name__ == '__main__':
-    app.run()
+    fact_connection()
